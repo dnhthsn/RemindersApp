45f760a010715a53c3814bf82a49818b584952dc
dnhthsn <dinhthaison2000@gmail.com>
2023-06-27 16:28:52 +0700

    first commit

--- RemindersApp/ViewController.swift
+++ RemindersApp/ViewController.swift
@@ -5,15 +5,114 @@
 //  Created by Đinh Thái Sơn on 27/06/2023.
 //
 
+import UserNotifications
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var table: UITableView!
+    
+    var models = [Reminder]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        table.delegate = self
+        table.dataSource = self
     }
+    
+    @IBAction func didTapAdd() {
+        guard let vc = storyboard?.instantiateViewController(identifier: "add") as? AddViewController else {
+            return
+        }
+        
+        vc.title = "New Reminder"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.completion = { title, body, date in
+            DispatchQueue.main.async {
+                self.navigationController?.popToRootViewController(animated: true)
+                let new = Reminder(title: title, date: date, identifier: "id_\(title)")
+                
+                self.models.append(new)
+                self.table.reloadData()
+                
+                let content = UNMutableNotificationContent()
+                content.title = title
+                content.sound = .default
+                content.body = body
+                
+                let targetDate = date
+                let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: targetDate), repeats: false)
+                
+                let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request, withCompletionHandler: {error in
+                    if error != nil {
+                        print("Something went wrong")
+                    }
+                })
+            }
+        }
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func didTapTest() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { success, error in
+            if success {
+                self.scheduleTest()
+            } else if error != nil {
+                print(error?.localizedDescription)
+            }
+        })
+    }
+    
+    func scheduleTest() {
+        let content = UNMutableNotificationContent()
+        content.title = "Hello"
+        content.sound = .default
+        content.body = "Body"
+        
+        let targetDate = Date().addingTimeInterval(5)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: targetDate), repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: {error in
+            if error != nil {
+                print("Something went wrong")
+            }
+        })
+    }
+}
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let date = models[indexPath.row].date
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM, dd, YYYY"
+        cell.textLabel?.text = models[indexPath.row].title
+        cell.detailTextLabel?.text = formatter.string(from: date)
+        
+        return cell
+    }
+}
+
+struct Reminder {
+    let title: String
+    let date: Date
+    let identifier: String
 }
 
